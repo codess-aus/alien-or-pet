d10d7923305b37c3ed84e55b703fb8bf8962a384
Michelle Mei-Ling Sandford <codesswa@outlook.com>
2023-06-20 16:23:57 +0800

    updates

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <DetectImage msg="Has your puppy joined the dark side?"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DetectImage from './components/DetectImage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DetectImage
   }
 }
 </script>
